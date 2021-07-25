ea5ce0d57f03c11e426f84c818c28efa5d36b7f3
Filipe Silva <filipeqs@outlook.com>
2021-07-25 10:33:49 -0400

    memebershiptypes enum

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -58,18 +58,28 @@ namespace Vidly.Controllers
             if (customer == null)
                 return HttpNotFound();
 
-            var ViewModel = new CustomerFormViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 Customer = customer,
                 MembershipTypes = _context.MembershipTypes.ToList()
             };
 
-            return View("CustomerForm", ViewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
