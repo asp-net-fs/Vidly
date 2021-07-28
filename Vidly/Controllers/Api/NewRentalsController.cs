94da1ff98fa2836e038bbbe5a447efd852ab8ee8
Filipe Silva <filipeqs@outlook.com>
2021-07-28 10:40:18 -0400

    newrentals

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            if (newRental.MoviesIds.Count == 0)
+                return BadRequest("No Movie Ids has been given.");
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+
+            if (customer == null)
+                return BadRequest("CustomerId is not valid.");
+
+            var movies = _context.Movies.Where(m => newRental.MoviesIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MoviesIds.Count)
+                return BadRequest("One or more MoviesIds are invalied.");
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
